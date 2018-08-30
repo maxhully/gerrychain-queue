12658836b9c583059b8ecf27a299002db7052102
Max Hully <max.hully@gmail.com>
2018-08-30 10:18:51 -0400

    Now actually ignore .vscode

--- gerrychain_queue/frontend.py
+++ gerrychain_queue/frontend.py
@@ -8,3 +8,8 @@ bp = Blueprint("frontend", __name__, url_prefix="/")
 def home():
     runs = list_runs()
     return render_template("index.html", runs=runs)
+
+
+@bp.route("/new")
+def create_run():
+    return render_template("new.html")
