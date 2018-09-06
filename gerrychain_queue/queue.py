9577f65ae75e39d54b2d95b2f09a29a8f316dac8
Max Hully <max.hully@gmail.com>
2018-09-06 11:38:24 -0400

    Fix report string issue

--- gerrychain_queue/queue.py
+++ gerrychain_queue/queue.py
@@ -68,4 +68,4 @@ class Queue:
         self.redis.publish(channel=task_key, message=message)
 
     def get_report(self, task_key):
-        return self.redis.get(task_key + "-report").decode("utf-8")
+        return json.loads(self.redis.get(task_key + "-report").decode("utf-8"))
